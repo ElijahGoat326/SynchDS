247c490fdc514c6a975b291673c947b80f0de593
ElijahGoat326 <elijahlopez326@gmail.com>
2023-05-15 14:18:17 -0400

    Added some more code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue myQ = new Queue();
+        myQ.put("elijah");
+        myQ.put("lopez");
+        myQ.put("hernandez");
+        for (int i = 0; i < 100; i = i + 1) {
+            myQ.put(i);
+
+        }
+        Object obj1 = myQ.get();
+        System.out.println(obj1);
+
+        for (int i = 0; i < 100; i = i + 1) {
+            Object obj2 = myQ.get();
+            System.out.println(obj1);
+        }
     }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Queue {
+public class Queue implements DataStructure{
+
+    int count;
+    int nextget;
+    int nextput = 0;
+
+
+    Object[] objects = new Object[100];
+    // Methods
+ public boolean put(Object obj) {
+     if (count < 100){
+         objects[nextput] = obj;
+     nextput = nextput + 1;
+     count = count + 1;
+     return true;
+ } else{
+     return false;
+    }
+  public Object get(){
+     Object obj = objects[nextget];
+
+     return obj;
+   }
+
+
+
+
+
+
 }
